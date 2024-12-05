ec781cebde2fdffc008d2ed1dd65e66c911db5ff
pavithra-0113 <pavithranagesh0111@gmail.com>
2024-12-05 15:02:34 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+ import Main from './Components/Main.js';
+ import './Components/style.css'
+
+ 
+  
+ 
+ function App() {
+   return (
+     <div> 
+      <Main/>
+
+     </div>
+   )
+ }
+ 
+ export default App
